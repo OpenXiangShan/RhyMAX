5fc0d3b51ee0cfafdaf127311259fdbfab99cf7c
leon <2279746395@qq.com>
2025-04-02 12:42:38 +0800

    更新Utility到主分支

--- src/main/scala/Adder/adder.scala
+++ src/main/scala/Adder/adder.scala
@@ -5,30 +5,30 @@ import utility.sram._
 
 import chisel3._
 
-// class Adder extends Module {
-//     val io = IO(new Bundle {
-//         val a = Input(UInt(8.W))
-//         val b = Input(UInt(8.W))
-//         val o = Output(UInt(8.W))
-//     })
-//     io.o := io.a + io.b
-// }
-
-
 class Adder extends Module {
-  val io = IO(new Bundle {
-    val a = Input(UInt(8.W))
-    val b = Input(UInt(8.W))
-    val o = Output(UInt(8.W))
+    val io = IO(new Bundle {
+        val a = Input(UInt(8.W))
+        val b = Input(UInt(8.W))
+        val o = Output(UInt(8.W))
+    })
+    io.o := io.a + io.b
+}
+
+
+// class Adder extends Module {
+//   val io = IO(new Bundle {
+//     val a = Input(UInt(8.W))
+//     val b = Input(UInt(8.W))
+//     val o = Output(UInt(8.W))
    
-  })
+//   })
 
-  val subFucku = Module(new Fucku())
+//   val subFucku = Module(new Fucku())
 
-  subFucku.io.a := io.a
-  subFucku.io.b := io.b
-  io.o := subFucku.io.o
+//   subFucku.io.a := io.a
+//   subFucku.io.b := io.b
+//   io.o := subFucku.io.o
 
 
   
-}
+// }
