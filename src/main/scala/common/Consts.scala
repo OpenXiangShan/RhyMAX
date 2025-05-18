ece311c406f2f8cf67cd9e2fc2cfb9eca0b4d02d
curryfromuestc <curryfromuestc@gmail.com>
2025-05-18 13:13:41 +0800

    添加fp8参数

--- src/main/scala/common/Consts.scala
+++ src/main/scala/common/Consts.scala
@@ -174,3 +174,30 @@ class RegFileFormat extends Module{
   val All_INDEX_LEN   = Consts.All_INDEX_LEN
 
 }
+
+object Float32 {
+  val LENGTH = 32
+  val expWidth = 8
+  val sigWidth = 23
+  val SHIFTED_LENGTH = 54
+}
+
+object Float8E4M3 {
+  val LENGTH = 8
+  val expWidth = 4
+  val sigWidth = 3
+}
+
+object FixedPoint {
+  val LENGTH = 9
+  val SIGN = 1
+  val FRACTION = 8
+  val FIRST_SHIFTED_LENGTH = 16
+  val SHIFTED_LENGTH = 45
+}
+
+object Parameters {
+  val WAY_SIZE = 8
+  val FP8_LENGTH = 8
+  val NUM_SCALE = 2
+}
